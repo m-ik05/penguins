d8b0b97eae27dfe997e52847de2aab544ad92340
m-ik05 <michal2005ik@gmail.com>
2025-02-15 15:37:27 +0100

    Delete interactive directory

--- interactive/headers/placement.h
+++
@@ -1,9 +0,0 @@
-#include "../headers/general.h"
-
-// Checks if selected ice floe is free
-// arguments = selected x and y coordinates and game board size
-int checkFloe(int, int, int, int);
-
-// Places penguin on the selected ice floe
-// arguments = x and y coordinates, the board and current player's number, pointer to the current player's structure
-void placePenguin(int, int, struct Tile[][MAX_SIZE], int, struct Player *);
